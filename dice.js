1ed7554795da52f145268613f6696f3623f3edf2
Ali <Ali@ADMINs-MacBook-Pro.local>
2022-05-23 10:12:21 +0300

    JS -- Get random numbers for each player

--- dice.js
+++ dice.js
@@ -1,5 +1,5 @@
 var get_random_index = function () {
-    var random_index = Math.floor(Math.random()*4);
+    var random_index = Math.floor(Math.random()*6);
     return(random_index);
     }
 
@@ -8,5 +8,9 @@ player_2 = document.getElementById('img_2');
 refresh = document.getElementById('refresh')
 
 var change_image = function () {
-    
+    player_1 = get_random_index()
+    player_2 = get_random_index()
+        console.log(player_1,player_2);
 }
+
+change_image()

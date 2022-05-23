64f1c0557543d70fd3702e4c9bd10e2f7acda3e2
Ali <Ali@ADMINs-MacBook-Pro.local>
2022-05-23 10:49:04 +0300

    JS -- Gets image of corresponding random number

--- dice.js
+++ dice.js
@@ -3,14 +3,36 @@ var get_random_index = function () {
     return(random_index);
     }
 
+
+var images_list = ["images/dice1.png","images/dice2.png","images/dice3.png","images/dice4.png","images/dice5.png","images/dice6.png"]
 player_1 = document.getElementById('img_1');
 player_2 = document.getElementById('img_2');
-refresh = document.getElementById('refresh')
+game_prompt = document.getElementsByTagName('h1');
 
 var change_image = function () {
     player_1 = get_random_index()
     player_2 = get_random_index()
-        console.log(player_1,player_2);
+    console.log(player_1,player_2);
+
+    document.getElementById("img_1").src = images_list[player_1];
+    document.getElementById("img_2").src = images_list[player_2];
 }
 
-change_image()
+var game = function() {
+    change_image()
+    if(player_1>player_2) {
+        document.getElementById('message').innerHTML    = "Player 1 Wins"
+        console.log('1 wins')
+    }
+    else if(player_1<player_2) {
+        document.getElementById('message').innerHTML    = "Player 2 Wins"
+        console.log('2 wins')
+    }
+    else {
+        document.getElementById('message').innerHTML    = "DRAW!"
+        console.log('draw')
+    }
+}
+
+game()
+

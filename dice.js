b80112393f0379be4a0b4a938695cbb8e519e3e2
Ali <Ali@ADMINs-MacBook-Pro.local>
2022-05-23 10:08:07 +0300

    Directory Initializing

---
+++ dice.js
@@ -0,0 +1,12 @@
+var get_random_index = function () {
+    var random_index = Math.floor(Math.random()*4);
+    return(random_index);
+    }
+
+player_1 = document.getElementById('img_1');
+player_2 = document.getElementById('img_2');
+refresh = document.getElementById('refresh')
+
+var change_image = function () {
+    
+}
